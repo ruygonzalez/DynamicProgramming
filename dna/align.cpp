d3b7b8bf8c195f52a8a9057580426da526d66b40
Aaron Feldman <feldmando@gmail.com>
2016-01-24 23:26:46 -0800

    Initial commit for 2016
    Eugene Bulkin has already done these updates since 2015:
        Update the due date to be correct for 2015-16.
    
        Rearrange assignment so that background goes with problems themselves as in
        older sets.

---
+++ dna/align.cpp
@@ -0,0 +1,131 @@
+//
+//  align.cpp
+//  dna_alignment
+//
+//  Created by Erika DeBenedictis on 1/27/13.
+//  Copyright (c) 2014 California Institute of Technology. All rights reserved.
+//
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+// scoring values
+#define GAP_SCORE -5
+#define MISMATCH -1
+#define MATCHING 2
+
+/**
+ * @brief Packages the score and instruction string the align function returns
+ */
+struct align_result {
+    int score;      // score of this alignment
+    string inst;    // instruction on how to align the inputs
+
+    align_result(int s, string i){
+      // constructor with values
+        this->score = s;
+        this->inst = i;
+    }
+    align_result(){
+        this->score = 0;
+        this->inst = "";
+    }
+};
+
+// memo_type will allow us to hash the string input to align
+// with its output for memoization
+typedef unordered_map<string, align_result> memo_type;
+
+/**
+ * @brief Function takes two strings, s and t, and produces an align_result
+ * of the highest alignment score and its corresponding instruction str.
+ */
+align_result align(string s, string t, memo_type &memo){
+  // if this result is memoized, use recorded result
+    string key = s + "," + t;
+    if (memo.count(key) > 0){
+      return memo[key];
+    }
+
+    /*
+      TODO: calculate the highest score for an alignment of s and t
+      - Base cases: s or t is empty
+      - Recursive calls
+     */
+
+    /* Before you return your calculated  align_result object,
+       memoize it like so:*/
+    align_result answer;
+    memo[key] = answer;
+    return answer;
+}
+
+/**
+ * @brief Wrapper function to print the results of align
+ */
+void DNA_align(string s, string t){
+    cout << endl<<"Calling DNA align on strings " << s <<", "<< t<< endl;
+
+    // create the memoization system
+    memo_type memo;
+
+    align_result answer = align(s,t, memo);
+    string ans =answer.inst;
+
+    // Printing section
+    string line1 = "";      // line where string s will be printed, spaces inserted
+    string line2 = "";      // line where string t will be printed, spaces inserted
+    string line3 = "";      // description of the relationship between s and t here (* | s t)
+
+    int j = 0;      // running index in s
+    int k = 0;      // running index in t
+
+    for (int m = 0; m< ans.length(); m++){
+
+        // i is the next element in our instruction string ans
+        string i = ans.substr(m, 1);
+
+        // only in s
+        if(i.compare("s") == 0){
+            line1 += s[j]; j++;
+            line2 += " ";
+            line3 += "s";
+        }
+
+        // only in t
+        else if (i.compare("t") == 0){
+            line1 += " ";
+            line2 += t[k]; k++;
+            line3 += "t";
+        }
+
+        // mismatch
+        else if (i.compare("*") == 0){
+            line1 += s[j]; j++;
+            line2 += t[k]; k++;
+            line3 += "*";
+        }
+
+        // match
+        else {
+            line1 += s[j]; j++;
+            line2 += t[k]; k++;
+            line3 += "|";
+        }
+    }
+    cout << line1 << endl << line2 << endl << line3 << endl;
+}
+
+int main(){
+  // some test cases to begin with
+    DNA_align("",   "a");
+    DNA_align("b",  "");
+    DNA_align("a", "a");
+    DNA_align("b",  "a");
+    DNA_align("b",  "ba");
+    DNA_align("ab", "ba");
+    DNA_align("ab", "b");
+    return 0;
+}
---
+++ seamcarve/src/SeamCarveAlgorithm.cpp
@@ -0,0 +1,63 @@
+/*
+ * SeamCarveAlgorithm.cpp
+ * Defines the seam carving algorithm.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following disclaimer
+ *   in the documentation and/or other materials provided with the
+ *   distribution.
+ * * Neither the name of the  nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ */
+
+#include "SeamCarveApp.hpp"
+
+#define min(x, y)           ((x) < (y) ? (x) : (y))
+
+
+/**
+ * @brief Peforms the seam carving algorithm.
+ *
+ * @param smap 2-d saliency map with width `w` and height `h`; can be
+ * indexed by `smap[i][j]`
+ *
+ * @param w Width of the saliency map
+ *
+ * @param h Height of the saliency map
+ */
+unsigned int *DoSeamCarve(unsigned int **smap, int w, int h)
+{
+    /* TODO: Write this function! */
+
+    unsigned int *seam = new unsigned int[h];
+
+    /* A very bad seam carving algorithm... */
+    for (int i = 0; i < h; i++)
+    {
+        seam[i] = 0;
+    }
+
+    return seam;
+}
+
